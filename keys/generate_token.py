62a73bdc989690cb6383547d607a48ac5b6ccb15
Lu1sTV <ehrlicher.luis@gmail.com>
2026-03-08 02:47:01 +0100

    implement podcast to global cloudbuild

--- keys/generate_token.py
+++ keys/generate_token.py
@@ -4,11 +4,11 @@ import os
 SCOPES = ["https://www.googleapis.com/auth/gmail.modify"]
 
 def main():
-    if not os.path.exists('credentials.json'):
-        print("FEHLER: credentials.json fehlt!")
+    if not os.path.exists('gmail_credentials.json'):
+        print("FEHLER: gmail_credentials.json fehlt!")
         return
 
-    flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+    flow = InstalledAppFlow.from_client_secrets_file('gmail_credentials.json', SCOPES)
     creds = flow.run_local_server(port=0)
 
     with open('token.json', 'w') as token:
